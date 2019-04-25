3839d12e1af0128d539cee08e580b73a72e8acab
Cameron Robinson <robinson.cameron90@gmail.com>
2019-04-25 11:13:39 -0400

    style and carousel

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ import Vue from 'vue';
 import VueCarousel from 'vue-carousel';
 
 Vue.use(VueCarousel);
+
+const app = new Vue ({
+  el: '#app',
+  data: {
+    image: 'https://images.unsplash.com/photo-1543364195-bfe6e4932397?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=701&q=80',
+    caption: 'caption',
+    altText:'',
+  },
+  methods: {
+    uploadPhoto() {
+        console.log('image', this.image);
+        console.log('caption',this.altText);
+        console.log('altText', this.altText);
+    }
+  }
+});
